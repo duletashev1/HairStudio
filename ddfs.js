628499f27091da3409e8a15ac164b182bfef3890
duletashev1 <116757605+duletashev1@users.noreply.github.com>
2023-07-25 19:44:48 +0200

    Add files via upload

---
+++ ddfs.js
@@ -0,0 +1,8 @@
+const menu=document.querySelector(".bottons");
+let btn=document.querySelector(".btn");
+function hidemenu(){
+    menu.style.display="none";
+}
+function showMenu(){
+    menu.style.display="block";
+}
